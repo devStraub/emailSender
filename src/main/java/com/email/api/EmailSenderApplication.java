cca392ce11e4fe8810bea414d8cc5de4796c5800
Michel Pech <michelhphz@hotmail.com>
2023-07-24 17:40:28 -0300

    update project

--- src/main/java/com/email/api/EmailSenderApplication.java
+++ src/main/java/com/email/api/EmailSenderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailSenderApiApplication {
+public class EmailSenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailSenderApiApplication.class, args);
+	    SpringApplication.run(EmailSenderApplication.class, args);
 	}
 
 }
